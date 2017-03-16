b9658c8957f622aa00c8b70d288ef80039acf624
uriel8 <dragon008_rojo@hotmail.com>
2017-03-16 01:01:58 -0600

    practica 10
    php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+    //recomendacion, poner comentarios a los codigos
+    //que se hagan
+    #comentario
+    /*comentario*/
+    echo "Hola mundo php";
+?>
+<html>
+    <head>
+        <title></title>
+    </head>
+    <body>
+        <h1><p>Hola mundo</p></h1>
+    </body>
+</html>
---
+++ index2.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <title>Practica 10 php</title>
+    </head>
+    <body>
+        <h1><p>Usando php</p></h1>
+        <p>
+            <?php
+            echo "Parrafo php";
+            ?>
+        </p>
+        <p>Parrafo HTML</p>
+        <h3>
+            <?php
+            echo "Otro Parrafo php\n
+            aqui un salto de linea";
+            ?>
+        </h3>
+    </body>
+</html>
